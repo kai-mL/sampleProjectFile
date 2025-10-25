ea88d37fe765d5b415f8ed2557c18bfdfaff4937
kai-mL <kakerusuzuki09tyoby@gmail.com>
2025-10-25 19:29:57 +0900

    function add

--- sampleProjectFile/ViewController.swift
+++ sampleProjectFile/ViewController.swift
@@ -9,13 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var button: UIButton!
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func add() {
+        
+    }
 
 }
 
