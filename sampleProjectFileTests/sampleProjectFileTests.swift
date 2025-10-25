2ff45a1277c02411f7fffbfabf1e360ab4f77bd0
kai-mL <kakerusuzuki09tyoby@gmail.com>
2025-10-25 19:25:30 +0900

    first commit

---
+++ sampleProjectFileTests/sampleProjectFileTests.swift
@@ -0,0 +1,17 @@
+//
+//  sampleProjectFileTests.swift
+//  sampleProjectFileTests
+//
+//  Created by 森掛 on 2025/10/25.
+//
+
+import Testing
+@testable import sampleProjectFile
+
+struct sampleProjectFileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
